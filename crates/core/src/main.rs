71974ed354f8b2ee1e887f32d1236284578ec845
Akash Shaw <shawakash2003@gmail.com>
2024-09-03 00:27:00 +0530

    chore: adds app state

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -5,27 +5,34 @@ use dotenv::dotenv;
 use env_logger::Env;
 use serde_json::json;
 
+pub mod routes;
+pub mod types;
+use crate::routes::contest::contest_config;
+use crate::types::AppState;
+
 use data_dust::fns::initialize_db_pool;
+
+async fn root() -> HttpResponse {
+    HttpResponse::Ok().json(json!({
+        "status": "Ok"
+    }))
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let db_pool = initialize_db_pool();
+    let app_state = web::Data::new(AppState {
+        db_pool: initialize_db_pool(),
+    });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(db_pool.clone()))
+            .app_data(app_state.clone())
             .wrap(Logger::default())
-            .route(
-                "/",
-                web::get().to(|| async {
-                    HttpResponse::Ok().json(json!({
-                        "status": "Ok"
-                    }))
-                }),
-            )
+            .configure(|cfg| contest_config(cfg, &app_state))
+            .route("/", web::get().to(root))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
