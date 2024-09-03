2540b2d8ccd5d2a5e6321306088e451d7ea42d72
Akash Shaw <shawakash2003@gmail.com>
2024-09-04 00:54:52 +0530

    chore: adds logic to handle subs part

--- crates/oxide_wave/src/main.rs
+++ crates/oxide_wave/src/main.rs
@@ -63,6 +63,74 @@ async fn handle_connection(stream: TcpStream, client: redis::Client) {
 
             match serde_json::from_str::<SubMsg>(msg) {
                 Ok(msg) => match msg.message_type.as_str() {
+                    "subscribe" => {
+                        let ws_sender_clone = Arc::clone(&ws_sender);
+                        let channel = msg.channel.clone();
+                        let client_clone = client.clone();
+                        let subscriptions_clone = Arc::clone(&subscriptions);
+
+                        let (tx, mut rx) = watch::channel(false);
+                        subscriptions_clone.lock().await.insert(channel.clone(), tx);
+
+                        tokio::spawn(async move {
+                            let pub_conn = client_clone
+                                .get_async_connection()
+                                .await
+                                .expect("Failed to connect to Redis for pubsub");
+                            let mut pubsub = pub_conn.into_pubsub();
+                            pubsub
+                                .subscribe(&channel)
+                                .await
+                                .expect("Failed to subscribe to channel");
+
+                            let task_pub_conn = client_clone
+                                .get_async_connection()
+                                .await
+                                .expect("Failed to connect to Redis for pubsub");
+                            let mut task_pubsub = task_pub_conn.into_pubsub();
+
+                            let mut message_stream = pubsub.on_message();
+
+                            loop {
+                                tokio::select! {
+                                    msg = message_stream.next() => {
+                                        if let Some(msg) = msg {
+
+                                            let payload: String =
+                                                msg.get_payload().expect("Failed to get payload");
+
+                                            let res = create_msg(
+                                                "data",
+                                                &channel,
+                                                Some(&payload.to_string()),
+                                            );
+
+                                            let mut sender = ws_sender_clone.lock().await;
+                                            sender
+                                                .send(Message::Text(res))
+                                                .await
+                                                .expect("Error sending message");
+                                        }
+                                    }
+                                    _ = rx.changed() => {
+                                        break;
+                                    }
+                                }
+                            }
+                            task_pubsub
+                                .unsubscribe(&channel)
+                                .await
+                                .expect("Failed to unsubscribe");
+                        });
+
+                        let res = create_msg("subscribed", &msg.channel, Some("Subscribed"));
+                        ws_sender
+                            .lock()
+                            .await
+                            .send(Message::Text(res))
+                            .await
+                            .expect("Error sending message");
+                    }
                     "unsubscribe" => {
                         let mut subs = subscriptions.lock().await;
                         if let Some(tx) = subs.remove(&msg.channel) {
